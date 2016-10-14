13a19c9b45f5df0d492d454877fb2e213649d465
schi631 <schi631@fhs.win.fuhsd.org>
2016-10-14 10:22:50 -0700

    added readFileAsString method

--- src/CSVdata.java
+++ src/CSVdata.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 import com.sun.org.apache.xpath.internal.operations.String;
 
 public class CSVdata {
@@ -38,6 +42,21 @@ public class CSVdata {
 		}
 		
 		
+	}
+
+	public String readFileAsString(String filepath) {
+		StringBuilder output = new StringBuilder();
+		
+		try(Scanner scanner = new Scanner(new File(filepath))){
+				
+				while(scanner.hasNext()){
+					java.lang.String line = scanner.nextLine();
+					output.append(line + System.getProperty("line.separator"));
+				}
+				
+		}catch(IOException e){
+			e.printStackTrace();
+		}return output.toString();
 	}
 
 	public static CSVdata readCSVFile(String filename, int numLinesToIgnore, String[] columnNames) {
