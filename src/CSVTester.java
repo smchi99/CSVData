a6eca130cb8c71d920feb6afdb2413b7d0db73f2
schi631 <schi631@fhs.win.fuhsd.org>
2016-10-14 11:12:30 -0700

    added to tester body

--- src/CSVTester.java
+++ src/CSVTester.java
@@ -3,10 +3,15 @@ import java.util.Arrays;
 public class CSVTester {
 	public static void main(String[]args){
 		String [] columnNames = {"time", "gyro-x", "gyro-y", "gyro-z"};
-		CSVdata data = new CSVdata("E:/java 2016/GyroTest2out.csv", columnNames, 1);
+		CSVdata data = new CSVdata("E:/java 2016/gyroTesting.txt", columnNames, 1);
+
 		
-		System.out.println(Arrays.toString(data.getIndividualRow(4)));
-		System.out.println(Arrays.toString(data.getIndividualCol(0)));
+		System.out.println(Arrays.toString(data.getIndividualRow(2)));
+		System.out.println(Arrays.toString(data.getIndividualCol(2)));
+		System.out.println(Arrays.toString(data.getMultipleRows(2,3)));
+		System.out.println(data.getTitleCol(3));
+		System.out.println(Arrays.toString(data.getIndividualCol("gyro-x")));
+		
 	}
 
 }
