fdba951820e0ae1ba2b2e1bb3f386e121a2d5a3d
schi631 <schi631@fhs.win.fuhsd.org>
2016-10-14 10:50:00 -0700

    added tester class

---
+++ src/CSVTester.java
@@ -0,0 +1,8 @@
+
+public class CSVTester {
+	public static void main(String[]args){
+		String [] columnNames = {"a","b","c"};
+		CSVdata data = new CSVdata("E:/java 2016/CSVdata/CSV.txt", columnNames, 0);
+	}
+
+}
